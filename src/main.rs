6fbcf20f57e17b6c12f00d1bb7b5a617542492aa
Marie Romanovich <marie.romanovich@gmail.com>
2025-09-27 15:56:30 +0200

    last

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ async fn send_startup_to_all(bot: &Bot) {
 
 Коротко о BLENDER — множество приваток, которые я лично отбирал с 2019 года и это самый дешевый и самый качественный агрегатор который вы могли только найти.
 
-Наш канал: t.me/blender
+Наш канал: [t.me/blender](https://t.me/+BtdRhhPojCZhMWM0)
 Поддержка: @ex_managers
 
 🔻 Сумма всех приваток: 7394$/мес
@@ -131,7 +131,7 @@ async fn send_startup_to_target(bot: &Bot) {
 
 Коротко о BLENDER — множество приваток, которые я лично отбирал с 2019 года и это самый дешевый и самый качественный агрегатор который вы могли только найти.
 
-Наш канал: t.me/blender
+Наш канал: [t.me/blender](https://t.me/+BtdRhhPojCZhMWM0)
 Поддержка: @ex_managers
 
 🔻 Сумма всех приваток: 7394$/мес
@@ -325,7 +325,7 @@ async fn greet(bot: &Bot, chat_id: ChatId, _st: &Value, pay: &payments::Payments
 
 Коротко о BLENDER — множество приваток, которые я лично отбирал с 2019 года и это самый дешевый и самый качественный агрегатор который вы могли только найти.
 
-Наш канал: t.me/blender
+Наш канал: [t.me/blender](https://t.me/+BtdRhhPojCZhMWM0)
 Поддержка: @ex_managers
 
 🔻 Сумма всех приваток: 7394$/мес
@@ -1390,7 +1390,7 @@ async fn send_startup_to_user(bot: &Bot, chat_id: ChatId) {
 
 Коротко о BLENDER — множество приваток, которые я лично отбирал с 2019 года и это самый дешевый и самый качественный агрегатор который вы могли только найти.
 
-Наш канал: t.me/blender
+Наш канал: [t.me/blender](https://t.me/+BtdRhhPojCZhMWM0)
 Поддержка: @ex_managers
 
 🔻 Сумма всех приваток: 7394$/мес
