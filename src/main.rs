5ec1bc113df9c0ca576d398049af265d1518403e
Marie Romanovich <marie.romanovich@gmail.com>
2025-09-27 16:10:34 +0200

    final

--- src/main.rs
+++ src/main.rs
@@ -1390,7 +1390,7 @@ async fn send_startup_to_user(bot: &Bot, chat_id: ChatId) {
 
 Коротко о BLENDER — множество приваток, которые я лично отбирал с 2019 года и это самый дешевый и самый качественный агрегатор который вы могли только найти.
 
-Наш канал: [t.me/blender](https://t.me/+BtdRhhPojCZhMWM0)
+Наш канал: https://t.me/+BtdRhhPojCZhMWM0
 Поддержка: @ex_managers
 
 🔻 Сумма всех приваток: 7394$/мес
